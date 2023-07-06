281ec382f4d5af2adb789c022889a3f1715e429e
Ashutosh Kumar Maurya <akmashh62@gmail.com>
2023-07-06 17:35:19 +0530

    db mein save hone lga

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
-const mongoURI = "mongodb://localhost:27017/inotebook";
+const mongoURI = ("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1");
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=> {
-        console.log("Connected to MongoDB successfully!!");
-    })
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then( ()=> console.log("Connected to MongoDB successfully!!"))
+    .catch((err=> console.log("the error is  : "+err)));
+
 }
 
 module.exports = connectToMongo;
--- models/Notes.js
+++ models/Notes.js
@@ -1,6 +1,7 @@
-const mongoose = required('mongoose');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const NotesSchema = new schema({
+const NotesSchema = new Schema({
     title:{
         type: String,
         required: true
